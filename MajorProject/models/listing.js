a6aac664e11fbfcc5a8473a18efe79573e2276ce
PranjaliKolawale <144488929+PranjaliKolawale@users.noreply.github.com>
2025-06-30 10:51:07 +0530

    Add files via upload

---
+++ MajorProject/models/listing.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        type: String,
+        set: (v) => v === "" ? "https://unsplash.com/photos/brown-wooden-house-in-the-middle-of-forest-during-daytime-jo8pclRHmCI": v,
+    },
+    price: Number,
+    location: String,
+    country: String
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
